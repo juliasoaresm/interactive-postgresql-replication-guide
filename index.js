185dd61077051bd4b55f0ffee99e616193ce7c5e
Julia <166180570+juliasoaresm@users.noreply.github.com>
2025-08-11 11:48:55 +0000

    Botão salvar

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function salvar() {
     if (document.getElementById("Versao").value > "18") {
         alert("Versão inválida, por favor, verifique a versão do seu sistema.");
     }
+    const salvo = document.querySelector("button.submit");
+    if (salvo) {
+        salvo.classList.remove("Salvar");
+        salvo.classList.add("fa-check");
+
+        setTimeout(() => {
+                salvo.classList.remove("fa-check");
+            salvo.classList.add("Salvar");
+        }, 1000);
+    }
 }
